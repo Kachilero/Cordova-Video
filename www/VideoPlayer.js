a4c74f369e955d1104ade7ff3ae63d3ff167263e
Alejandro <alejandrocerro@Alejandros-MacBook-Pro.local>
2015-04-06 10:29:40 -0500

    added functions for success and failure

--- www/VideoPlayer.js
+++ www/VideoPlayer.js
@@ -20,7 +20,11 @@ var exec = require("cordova/exec");
 
 var VideoPlayer = {
   play: function(url) {
-    exec(null, null, "VideoPlayer", "playVideo", [url]);
+    exec(function(result){
+      console.log("VideoPlayer Success Function " + result);
+    }, function(error){
+      console.log("VideoPlayer Error Function " + error);
+    }, "VideoPlayer", "playVideo", [url]);
   }
 };
 
